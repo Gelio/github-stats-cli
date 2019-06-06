ee950bd331dc30be3d8e1650c3fe3636091f8ef3
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-06-06 17:50:42 +0200

    add query factories

---
+++ src/queries.js
@@ -0,0 +1,81 @@
+const commonQueryPart = `
+nodes {
+  ... on PullRequest {
+    number
+    permalink
+    title
+    updatedAt
+    changedFiles
+    additions
+    deletions
+    author {
+      login
+    }
+    reviews(first: $firstReviews) {
+      nodes {
+        author {
+          login
+        }
+        comments {
+          totalCount
+        }
+        publishedAt
+        state
+      }
+    }
+  }
+}
+pageInfo {
+  endCursor
+  hasNextPage
+}`;
+
+/**
+ * @param {string} repository
+ * @param {string} timeRange
+ */
+function getInitialRequestInformation(repository, timeRange) {
+  return {
+    query: `
+query GetRepositoryInfo($firstPrs: Int!, $firstReviews: Int!, $query: String!) {
+search(first: $firstPrs, type: ISSUE, query: $query) {
+  ${commonQueryPart}
+}
+}
+`,
+    variables: {
+      firstPrs: 100,
+      firstReviews: 100,
+      query: `type:pr repo:${repository} updated:${timeRange} sort:updated-desc`
+    }
+  };
+}
+
+/**
+ *
+ * @param {string} repository
+ * @param {string} timeRange
+ * @param {string} pageCursor
+ */
+function getIntermediateRequestInformation(repository, timeRange, pageCursor) {
+  return {
+    query: `
+query GetRepositoryInfo($afterCursor: String!, $firstPrs: Int!, $firstReviews: Int!, $query: String!) {
+search(first: $firstPrs, after: $afterCursor, type: ISSUE, query: $query) {
+  ${commonQueryPart}
+}
+}
+`,
+    variables: {
+      afterCursor: pageCursor,
+      firstPrs: 100,
+      firstReviews: 100,
+      query: `type:pr repo:${repository} updated:${timeRange} sort:updated-desc`
+    }
+  };
+}
+
+module.exports = {
+  getInitialRequestInformation,
+  getIntermediateRequestInformation
+};
