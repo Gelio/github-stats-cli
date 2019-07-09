31e1bad0c9d89f9ad19e279e7f187ad73ff6bc7b
Grzegorz Rozdzialik <grzegorz.rozdzialik@codilime.com>
2019-07-09 12:42:59 -0700

    add filtering reviews by timestamp

--- fetch-data.ts
+++ fetch-data.ts
@@ -1,10 +1,11 @@
 require('dotenv').config();
 
 import { writeFileSync } from 'fs';
+import * as createGraphQlClient from 'graphql-client';
+
 import { fetchReviewStats } from './src/fetch-review-stats';
 import { config } from './src/config';
-
-import * as createGraphQlClient from 'graphql-client';
+import { PullRequest } from './src/types';
 
 const client = createGraphQlClient({
   url: 'https://api.github.com/graphql',
@@ -13,7 +14,24 @@ const client = createGraphQlClient({
   }
 });
 
+function filterReviews(prs: PullRequest[]) {
+  const [fromTime, toTime] = config.timeRange.split('..');
+  const fromTimestamp = new Date(fromTime).getTime();
+  const toTimestamp = new Date(toTime).getTime();
+
+  return prs.map(pr => {
+    pr.reviews.nodes = pr.reviews.nodes.filter(review => {
+      const timestamp = new Date(review.publishedAt).getTime();
+
+      return timestamp >= fromTimestamp && timestamp <= toTimestamp;
+    });
+
+    return pr;
+  });
+}
+
 fetchReviewStats(client, config.repository, config.timeRange)
+  .then(filterReviews)
   .then(body => {
     console.log(`Finished fetching. Saving to ${config.fileName}`);
     writeFileSync(config.fileName, JSON.stringify(body, null, 2), 'utf8');
